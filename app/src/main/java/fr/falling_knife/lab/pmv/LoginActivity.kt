0b406fa6da5587d16fe03735422e4b1b6c6b98ed
Fallingknife84 <dev.erwanbo@caramail.fr>
2022-02-22 13:36:55 +0100

    Fixed Launch Bug

--- app/src/main/java/fr/falling_knife/lab/pmv/LoginActivity.kt
+++ app/src/main/java/fr/falling_knife/lab/pmv/LoginActivity.kt
@@ -1,13 +1,30 @@
 package fr.falling_knife.lab.pmv
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_MESSAGE = "fr.falling_knife.lab.pmv.MESSAGE"
 
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+
+    fun onBtnLogin(view: View){
+        val info = Toast.makeText(this, "Vérification des identifiants...", Toast.LENGTH_LONG)
+        info.show()
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, "Coucou")
+        }
+
+        startActivity(intent)
     }
 
 
